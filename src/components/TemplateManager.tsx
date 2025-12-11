c1d1adaed514fb5c66a315be58897cbb367e4455
batjuancrespo <batjuancrespo@gmail.com>
2025-12-11 09:09:28 +0100

    Increase bottom padding to pb-24 for template scroll area

--- src/components/TemplateManager.tsx
+++ src/components/TemplateManager.tsx
@@ -116,7 +116,7 @@ export function TemplateManager({ onInsert, onClose }: TemplateManagerProps) {
                 </div>
             )}
 
-            <div className="flex-1 overflow-y-auto p-4 pb-8">
+            <div className="flex-1 overflow-y-auto p-4 pb-24">
                 {viewMode === 'list' ? (
                     <div className="space-y-4">
                         <button
