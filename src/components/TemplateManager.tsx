b3062de5eda1a8d6b54d8d943ba91b56c18ff5b7
batjuancrespo <batjuancrespo@gmail.com>
2025-12-11 09:07:20 +0100

    Add extra bottom padding to template modal scroll area

--- src/components/TemplateManager.tsx
+++ src/components/TemplateManager.tsx
@@ -116,7 +116,7 @@ export function TemplateManager({ onInsert, onClose }: TemplateManagerProps) {
                 </div>
             )}
 
-            <div className="flex-1 overflow-y-auto p-4">
+            <div className="flex-1 overflow-y-auto p-4 pb-8">
                 {viewMode === 'list' ? (
                     <div className="space-y-4">
                         <button
